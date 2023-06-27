229e3ff672a4df8edd74ab4f79a4cadba102cf2c
AbhiAdhav007 <120732315+AbhiAdhav007@users.noreply.github.com>
2023-06-27 13:26:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
-
-function palindrome(str){
-
+function Palindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  str = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
+  
+  // Check if the string is equal to its reverse
+  return str === str.split('').reverse().join('');
 }
+
+// console.log(isPalindrome("race a car")); // false
+// console.log(isPalindrome("racecar")); // true
+// console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+
 module.exports = palindrome
